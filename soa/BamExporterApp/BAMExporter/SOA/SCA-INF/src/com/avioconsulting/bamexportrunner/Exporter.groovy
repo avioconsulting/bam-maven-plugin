5a51613de6416b27b721d4cefa6e5f900acb5d96
Brady Wied <brady@wied.us>
2017-01-14 14:49:30 -0700

    Pass BAM project into code

--- soa/BamExporterApp/BAMExporter/SOA/SCA-INF/src/com/avioconsulting/bamexportrunner/Exporter.groovy
+++ soa/BamExporterApp/BAMExporter/SOA/SCA-INF/src/com/avioconsulting/bamexportrunner/Exporter.groovy
@@ -1,7 +1,11 @@
 package com.avioconsulting.bamexportrunner
 
+import oracle.xml.parser.v2.XMLElement
+
 class Exporter {
-    static def test() {
-        println 'YES WE RAN!!!*************************'
+    static def test(XMLElement input) {
+        def bamProjectNode = input.getElementsByTagName('bamProject').item(0)
+        def bamProject = bamProjectNode.textContent
+        println "we will export BAM project ${bamProject}"
     }
 }
