dd70470ad7d198a61db594918f305fb971c181d5
Brady Wied <brady@wied.us>
2017-01-14 13:37:52 -0700

    Move maven plugin in here

---
+++ bam-maven-plugin/src/main/groovy/com/avioconsulting/bamexporter/Tester.groovy
@@ -0,0 +1,16 @@
+package com.avioconsulting.bamexporter
+
+import com.avioconsulting.bamexporterservice.BamexporterprocessClientEp
+
+import javax.xml.ws.BindingProvider
+
+class Tester {
+    public static void main(String[] args) {
+
+        def service = new BamexporterprocessClientEp()
+        def port = service.bamExporterProcessPt
+        ((BindingProvider) port).requestContext[BindingProvider.ENDPOINT_ADDRESS_PROPERTY] = 'http://localhost:8001/soa-infra/services/default/BAMExporter/bamexporterprocess_client_ep'
+        def result = port.process 'howdy'
+        result.writeTo(new FileOutputStream("stuff.zip"))
+    }
+}
