67498daa30d47a0b95668372a591f8ad959c938e
Brady Wied <brady@wied.us>
2017-01-14 19:44:38 -0700

    Add in Maven plugin stuff

--- bam-maven-plugin/src/main/groovy/com/avioconsulting/bamexporter/Tester.groovy
+++ bam-maven-plugin/src/main/groovy/com/avioconsulting/bamexporter/Tester.groovy
@@ -6,7 +6,6 @@ import javax.xml.ws.BindingProvider
 
 class Tester {
     public static void main(String[] args) {
-
         def service = new BamexporterprocessClientEp()
         def port = service.bamExporterProcessPt
         ((BindingProvider) port).requestContext[BindingProvider.ENDPOINT_ADDRESS_PROPERTY] = 'http://localhost:8001/soa-infra/services/default/BAMExporter/bamexporterprocess_client_ep'
